d6f01758f5bcd065e896a9e4388bf0ca295566fb
catroll <kwicoo@gmail.com>
2013-12-21 12:53:51 +0800

    BUG:os not import

--- zhidewen/settings.py
+++ zhidewen/settings.py
@@ -79,7 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'static'),
+    join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -119,7 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'templates')
+    join(SITE_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
