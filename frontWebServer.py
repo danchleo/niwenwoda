a3291412c36a9557964ae1f88ba6edc8b2b94279
ZhangYao <mystorp@gmail.com>
2013-12-23 01:56:07 +0800

    添加主菜单鼠标悬浮动画，调整主菜单内部及 Tab 项样式

---
+++ frontWebServer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: 张垚
+#
+# 我在本地调试的时候遇到服务器端大修改，导致服务器不可用，所以就写了这个仅供
+# 前台开发使用的服务器，前台调试的过程中也可以使用此服务器，添加一些 JSON 文
+# 就 OK 了，等后台完善后再切换回去
+
+
+import SimpleHTTPServer
+
+SimpleHTTPServer.test()
